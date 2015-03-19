916d236df9ce981fb1c8c66c76cdf85984e60bba
frepe698 <fredrik.petre@hotmail.com>
2015-03-19 17:11:03 +0100

    VOLLEY

--- TileTerrain/Assets/Scripts/Game/GameMaster.cs
+++ TileTerrain/Assets/Scripts/Game/GameMaster.cs
@@ -74,7 +74,7 @@ public class GameMaster : MonoBehaviour {
 		}
 		spawnHeroes();
         setPlayerUnit(playerUnitID);
-        getHero(playerUnitID).learnAbility("rangedbasicattack");
+        getHero(playerUnitID).learnAbility("volley");
         getHero(playerUnitID).learnAbility("heavystrike");
 	    //world.addAnimals();
 
